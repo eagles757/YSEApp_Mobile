2946c4c316ace25a25d119d63d41193d84c1a3ce
jmercier <jmercier@affinitylivinggroup.onmicrosoft.com>
2021-08-31 15:01:50 -0400

    Added scannstuff

--- YSEApp/YSEApp/MainPage.xaml.cs
+++ YSEApp/YSEApp/MainPage.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System.ComponentModel;
+using System.Data;
 using Xamarin.Forms;
 using ZXing;
 
@@ -9,6 +10,10 @@ namespace YSEApp
     [DesignTimeVisible(false)]
     public partial class MainPage : ContentPage
     {
+        YSEClass gl = new YSEClass();
+        string connstr = "server=a2nlmysql39plsk.secureserver.net;uid=Eagles757;pwd=Joker321@@;database=ph14846596325_EagleSales;persistsecurityinfo=True;SslMode=none";
+
+
         public MainPage()
         {
            // ZXing.Net.Mobile.Forms.iOS.Platform.Init();
@@ -18,8 +23,39 @@ namespace YSEApp
         {
             Device.BeginInvokeOnMainThread(async () =>
             {
-                await DisplayAlert("Scanned result", result.Text, "OK");
+                //await DisplayAlert("Scanned result", result.Text, "OK");
+
+                EagleItem i = new EagleItem();
+
+                string qry = "select * from ph14846596325_EagleSales.EagleSales_Item as a left join ph14846596325_EagleSales.EagleSales_ItemGroup as b on b.ItemNumber = a.ItemNumber left join(select ItemNumber, Indv_Cost from ph14846596325_EagleSales.EagleSales_Transact order by t_ID desc) as c on c.ItemNumber = a.ItemNumber left join(select* from  ph14846596325_EagleSales.EagleSales_Pricing where enddate = '1/1/1900') as d on d.ItemNumber = a.ItemNumber left join(select* from  ph14846596325_EagleSales.EagleSales_CurrentOnHand) as e on e.ItemNumber = a.ItemNumber where a.ItemNumber = " + result.Text + "'";
+
+                DataSet ds = gl.Mysql_run(qry, connstr);
+
+                foreach (DataRow a in ds.Tables[0].Rows)
+                {
+                    i.ItemNumber = a["a.ItemNumber"].ToString();
+                    i.SKU = a["SKU"].ToString();
+                    i.Name = a["Name"].ToString();
+                    i.Supplier = a["Supplier"].ToString();
+                    i.ImageUrl = a["ImageUrl"].ToString();
+                    i.ProductUrl = a["ProductUrl"].ToString();
+                    i.PrimaryCat = a["MainGroupID"].ToString();
+                    i.SubCat = a["SubGroupID"].ToString();
+                    i.Cost = a["Indv_Cost"].ToString();
+                    i.Retail = a["Price"].ToString();
+                    i.NumberInStock = a["NumberInStock"].ToString();
+                    break;
+                }
+                string results_t = "Item Number: " + i.ItemNumber + " - " + i.Name + " [" + i.SKU + "]. Retail: " + i.Retail + ".  Cost: " + i.Cost + ". Number in stock: " + i.NumberInStock;
+
+
+
+                await DisplayAlert("Scanned result",  results_t, "OK");
+
+
             });
         }
+        
     }
+
 }
---
+++ YSEApp/YSEApp/YSEClass.cs
@@ -0,0 +1,147 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Text;
+
+namespace YSEApp
+{
+    class YSEClass
+    {
+        public string Mysql_update(string query, string constring)
+        {
+            try
+            {
+                using (MySqlConnection connection = new MySqlConnection(constring))
+                {
+                    using (MySqlCommand command = new MySqlCommand(query, connection))
+                    {
+                        try
+                        {
+                            connection.Open();
+                            string return1 = command.ExecuteNonQuery().ToString();
+                            connection.Close();
+                            return return1;
+                        }
+                        catch (Exception ex)
+                        {
+                            // Response.Write("<BR> ERROR 35: " & ex.ToString)
+                            return "ERROR|" + ex.ToString();
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                return null;
+            }
+        }
+
+        public DataSet Mysql_run(string query, string constring)
+        {
+            DataSet ds = new DataSet();
+            DataSet ds2 = new DataSet();
+            MySqlDataAdapter da;
+            try
+            {
+                var connStr = constring;
+
+
+                using (MySqlConnection connection = new MySqlConnection(connStr))
+                {
+                    using (MySqlCommand command = new MySqlCommand(query, connection))
+                    {
+                        try
+                        {
+                            connection.Open();
+                            command.CommandTimeout = 300;
+                            int iCount = 0;
+
+                            da = new MySqlDataAdapter(command);
+                            da.Fill(ds);
+                            connection.Close();
+                            return ds;
+                        }
+                        catch (Exception ex)
+                        {
+                            // Response.Write("<BR> Error 35 " & ex.ToString)
+                            // log("CALL " & query & " ------ SQL_RUL DLL ERROR: " & ex.ToString, True, "Y", "C:\FEED\logs\ALG_DLL_ERROR_" & DateTime.Now.ToString("MM-dd-yyyy") & ".txt")
+                            return null/* TODO Change to default(_) if this is not a reference type */;
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                // log("CALL " & query & " ------ SQL_RUL DLL ERROR: " & ex.ToString, True, "Y", "C:\FEED\logs\ALG_DLL_ERROR_" & DateTime.Now.ToString("MM-dd-yyyy") & ".txt")
+                DataSet ds1 = new DataSet();
+                ds1.Tables[0].Columns.Add("ERROR");
+                ds1.Tables[0].Rows.Add("ERROR " + ex.ToString());
+                return ds1;
+            }
+        }
+    }
+    public class WLItem
+    {
+        string ItemNumber  { get; set;}
+        string Qty { get; set; }
+        string Price { get; set; }
+        string ItemName { get; set; }
+    }
+
+    public class EagleItem
+    {
+
+
+        public string ItemNumber { get; set; }
+        public string SKU { get; set; }
+        public string Name { get; set; }
+        public string Supplier { get; set; }
+        public string ImageUrl { get; set; }
+        public string ProductUrl { get; set; }
+        public string PrimaryCat { get; set; }
+        public string SubCat { get; set; }
+        public string Cost { get; set; }
+        public string Retail { get; set; }
+        public string NumberInStock { get; set; }
+        YSEClass gl = new YSEClass();
+
+        string saveItem(String connstr)
+        {
+
+
+            string rtn = "";
+            string qry = "";
+            if(ItemNumber == "") {
+                qry = "insert into ph14846596325_EagleSales.EagleSales_Item ( SKU, Name, Supplier, ImageUrl, ProductUrl,Cost) values ('" + SKU + "', '" + Name + "', '" + Supplier + "', '" + ImageUrl + "', '" + ProductUrl + "', '" + Cost + "'); SELECT LAST_INSERT_ID(); ";
+
+                DataSet ds = gl.Mysql_run(qry, connstr);
+                
+                foreach(DataRow a in ds.Tables[0].Rows)
+                {
+                    ItemNumber = a["LAST_INSERT_ID"].ToString();
+                }
+                
+
+                if(ItemNumber != ""){
+                    qry = "insert into ph14846596325_EagleSales.EagleSales_ItemGroup (ItemNumber, MainGroupID, SubGroupID, StartDate, EndDate) values('" + ItemNumber + "', '" + PrimaryCat + "', '" + SubCat + "', '" + DateTime.Now.Date.ToString("yyyy/MM/dd") + "', '1-1-1900'); ";
+                    ds = gl.Mysql_run(qry, connstr);
+                    foreach(DataRow a  in ds.Tables[0].Rows){
+                                   rtn = ItemNumber;
+                    }
+
+                }
+            }
+        
+            else {
+                //'update 
+            }
+
+            return rtn;
+
+        }
+    }
+
+
+
+}
