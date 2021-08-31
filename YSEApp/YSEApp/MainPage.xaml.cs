e56d65f464ed6be4762d44c7f195a52dd9bb6fb3
eagles757 <70713192+eagles757@users.noreply.github.com>
2021-08-30 21:39:44 -0400

    Add project files.

---
+++ YSEApp/YSEApp/MainPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel;
+using Xamarin.Forms;
+using ZXing;
+
+namespace YSEApp
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+           // ZXing.Net.Mobile.Forms.iOS.Platform.Init();
+            InitializeComponent();
+        }
+        public void Handle_OnScanResult(Result result)
+        {
+            Device.BeginInvokeOnMainThread(async () =>
+            {
+                await DisplayAlert("Scanned result", result.Text, "OK");
+            });
+        }
+    }
+}
