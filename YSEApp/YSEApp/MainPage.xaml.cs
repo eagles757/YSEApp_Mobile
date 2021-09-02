49e35c825fbb22ee63a85e171c889a8f2e3c9d67
jmercier <jmercier@affinitylivinggroup.onmicrosoft.com>
2021-09-02 16:45:45 -0400

    Added API Usage

--- YSEApp/YSEApp/MainPage.xaml.cs
+++ YSEApp/YSEApp/MainPage.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System.ComponentModel;
+﻿using Newtonsoft.Json;
+using System.ComponentModel;
 using System.Data;
+using System.Net.Http;
 using Xamarin.Forms;
 using ZXing;
 
@@ -11,8 +13,9 @@ namespace YSEApp
     public partial class MainPage : ContentPage
     {
         YSEClass gl = new YSEClass();
-        string connstr = "server=a2nlmysql39plsk.secureserver.net;uid=Eagles757;pwd=Joker321@@;database=ph14846596325_EagleSales;persistsecurityinfo=True;SslMode=none";
-
+        // string connstr = "server=a2nlmysql39plsk.secureserver.net;uid=Eagles757;pwd=Joker321@@;database=ph14846596325_EagleSales;persistsecurityinfo=True;SslMode=none";
+        EagleItem i = new EagleItem();
+        HttpClient client;
 
         public MainPage()
         {
@@ -25,27 +28,35 @@ namespace YSEApp
             {
                 //await DisplayAlert("Scanned result", result.Text, "OK");
 
-                EagleItem i = new EagleItem();
 
-                string qry = "select * from ph14846596325_EagleSales.EagleSales_Item as a left join ph14846596325_EagleSales.EagleSales_ItemGroup as b on b.ItemNumber = a.ItemNumber left join(select ItemNumber, Indv_Cost from ph14846596325_EagleSales.EagleSales_Transact order by t_ID desc) as c on c.ItemNumber = a.ItemNumber left join(select* from  ph14846596325_EagleSales.EagleSales_Pricing where enddate = '1/1/1900') as d on d.ItemNumber = a.ItemNumber left join(select* from  ph14846596325_EagleSales.EagleSales_CurrentOnHand) as e on e.ItemNumber = a.ItemNumber where a.ItemNumber = " + result.Text + "'";
 
-                DataSet ds = gl.Mysql_run(qry, connstr);
+                //string qry = "select * from ph14846596325_EagleSales.EagleSales_Item as a left join ph14846596325_EagleSales.EagleSales_ItemGroup as b on b.ItemNumber = a.ItemNumber left join(select ItemNumber, Indv_Cost from ph14846596325_EagleSales.EagleSales_Transact order by t_ID desc) as c on c.ItemNumber = a.ItemNumber left join(select* from  ph14846596325_EagleSales.EagleSales_Pricing where enddate = '1/1/1900') as d on d.ItemNumber = a.ItemNumber left join(select* from  ph14846596325_EagleSales.EagleSales_CurrentOnHand) as e on e.ItemNumber = a.ItemNumber where a.ItemNumber = " + result.Text + "'";
 
-                foreach (DataRow a in ds.Tables[0].Rows)
+                //DataSet ds = gl.Mysql_run(qry, connstr);
+
+                //foreach (DataRow a in ds.Tables[0].Rows)
+                //{
+                //    i.ItemNumber = a["a.ItemNumber"].ToString();
+                //    i.SKU = a["SKU"].ToString();
+                //    i.Name = a["Name"].ToString();
+                //    i.Supplier = a["Supplier"].ToString();
+                //    i.ImageUrl = a["ImageUrl"].ToString();
+                //    i.ProductUrl = a["ProductUrl"].ToString();
+                //    i.PrimaryCat = a["MainGroupID"].ToString();
+                //    i.SubCat = a["SubGroupID"].ToString();
+                //    i.Cost = a["Indv_Cost"].ToString();
+                //    i.Retail = a["Price"].ToString();
+                //    i.NumberInStock = a["NumberInStock"].ToString();
+                //    break;
+                //}
+                HttpResponseMessage response = await client.GetAsync("https://www.yseusa.com//WebAPI/YSEA.php?YSEKey=YSE1234YSE&ItemNum=" + result.Text);
+                if (response.IsSuccessStatusCode)
                 {
-                    i.ItemNumber = a["a.ItemNumber"].ToString();
-                    i.SKU = a["SKU"].ToString();
-                    i.Name = a["Name"].ToString();
-                    i.Supplier = a["Supplier"].ToString();
-                    i.ImageUrl = a["ImageUrl"].ToString();
-                    i.ProductUrl = a["ProductUrl"].ToString();
-                    i.PrimaryCat = a["MainGroupID"].ToString();
-                    i.SubCat = a["SubGroupID"].ToString();
-                    i.Cost = a["Indv_Cost"].ToString();
-                    i.Retail = a["Price"].ToString();
-                    i.NumberInStock = a["NumberInStock"].ToString();
-                    break;
+                    string content = await response.Content.ReadAsStringAsync();
+                    i = JsonConvert.DeserializeObject<EagleItem>(content);
                 }
+
+
                 string results_t = "Item Number: " + i.ItemNumber + " - " + i.Name + " [" + i.SKU + "]. Retail: " + i.Retail + ".  Cost: " + i.Cost + ". Number in stock: " + i.NumberInStock;
 
 
